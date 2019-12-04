28a51674287643bf8bf767f0ee5745065ac88db0
fred512 <fredborges512@gmail.com>
2019-12-04 19:52:08 -0300

    Teste de alteração

--- src/components/item.vue
+++ src/components/item.vue
@@ -22,6 +22,7 @@
         </div>
       </div> 
     </div>
+    teste de alteração
     <div class="tr" v-for="(item,i) in itens" :key="i" @keyup.esc="saiSemGravar(item)" 
         :class="{'composicao':item.composicao,'erro':item.erro.length,'LinhaPar':i%2==0,
         'Selected':item.selecionado,'alerta':item.alerta.length,
