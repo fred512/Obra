f262c5cf468de4bf0e5a1396b16759b970a5b14c
fred512 <fredborges512@gmail.com>
2019-12-10 09:37:57 -0300

    ajuste

--- src/App.vue
+++ src/App.vue
@@ -225,17 +225,20 @@ body{
   grid-area: banner;
   background-color: #4472C4;
   margin:5% ;
+  border-radius: 5px;
+}
+.banner div{
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   margin-right: 0;
-  border-radius: 5px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  height:calc(100%) !important;
 }
-.banner img{
+.banner > div > img{
   width: 90%;
+  flex:1;
   height: auto;
   margin: 1vh auto;
   border-radius: 5px;
